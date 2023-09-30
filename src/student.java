7d84f4a93739e9961fa69779ed233c63128155b2
Pikott143 <theunseenmaiden@gmail.com>
2023-09-30 12:21:26 +0800

    added initial files

---
+++ src/student.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PC
+ */
+public class student {
+    String name;
+    String section;
+    String motto;
+    
+    public String getName(){
+        
+        return name;
+    }    
+    public String setSection(){
+        
+        return "IT21C";
+    }
+    public String getMotto(){
+        
+        return "Time is gold";
+ }
+ } 
+
